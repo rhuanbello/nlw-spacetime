33754891df9f3a1d27551f0365e10bff3f1f4955
rhuanbello <rhuambello@gmail.com>
2023-05-23 08:23:32 -0300

    [feature] Web Redirect Auth & Components

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,7 @@
-import { User } from 'lucide-react'
-import Image from 'next/image'
-import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+import { CopyRight } from '@/components/Copyright'
+import { EmptyMemories } from '@/components/EmptyMemories'
+import { Hero } from '@/components/Hero'
+import { SignIn } from '@/components/SignIn'
 
 export default function Home() {
   return (
@@ -10,63 +11,13 @@ export default function Home() {
 
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
-        <a
-          href=""
-          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
-        >
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-            <User className="h-5 w-5 text-gray-500" />
-          </div>
-
-          <p className="max-w-[140px] text-sm leading-snug">
-            <span className="underline">Crie sua conta</span> e salve suas
-            memórias!
-          </p>
-        </a>
-
-        <div className="space-y-5">
-          <Image src={nlwLogo} alt="Logo" draggable={false} />
-
-          <div className="max-w-[420px] space-y-1">
-            <h1 className="text-5xl font-bold leading-tight text-gray-50">
-              Sua cápsula do tempo
-            </h1>
-            <p className="text-lg leading-relaxed">
-              Colecione momentos marcantes da sua jornada e compartilhe (se
-              quiser) com o mundo!
-            </p>
-          </div>
-
-          <a
-            href=""
-            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
-          >
-            Cadastrar Lembrança
-          </a>
-        </div>
-
-        <div>
-          Feito com 💜 no NLW da{' '}
-          <a
-            target="_blank"
-            href="https://github.com/rhuanbello"
-            className="underline hover:text-gray-100"
-            rel="noreferrer"
-          >
-            Rocketseat por @rhuanbello
-          </a>
-        </div>
+        <SignIn />
+        <Hero />
+        <CopyRight />
       </div>
 
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-        <div className="flex flex-1 items-center justify-center">
-          <p className="w-[360px] text-center leading-relaxed">
-            Você ainda não registrou nenhuma lembrança, comece a{' '}
-            <a href="" className="underline hover:text-gray-50">
-              criar agora!
-            </a>
-          </p>
-        </div>
+        <EmptyMemories />
       </div>
     </main>
   )
---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export const CopyRight = () => {
+  return (
+    <div>
+      Feito com 💜 no NLW da{' '}
+      <a
+        target="_blank"
+        href="https://github.com/rhuanbello"
+        className="underline hover:text-gray-100"
+        rel="noreferrer"
+      >
+        Rocketseat por @rhuanbello
+      </a>
+    </div>
+  )
+}
