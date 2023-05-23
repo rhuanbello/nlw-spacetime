f2047a0e7f22097da509e9bc56684cba4c9e8e8a
rhuanbello <rhuambello@gmail.com>
2023-05-23 09:09:07 -0300

    [feature] Server Access Token Exchange & Save User on DB

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
+import 'dotenv/config'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
@@ -9,6 +11,7 @@ app.register(cors, {
 })
 
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
