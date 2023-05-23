f1f43970f84493b02c68970e904760b10c53dc7a
rhuanbello <rhuambello@gmail.com>
2023-05-23 20:50:16 -0300

    [feature] Mobile Authentication and Route Redirection

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,28 +1,72 @@
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
-
+import { useRouter } from 'expo-router'
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
 
+import * as SecureStore from 'expo-secure-store'
+
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
-import blurBg from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripes.svg'
-import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 
 import { styled } from 'nativewind'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import { useEffect } from 'react'
+import { api } from '../src/lib/api'
 
 const StyledStripes = styled(Stripes)
 
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/58739fbea2074e0a5c9a',
+}
+
 export default function App() {
+  const router = useRouter()
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  const [, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: '58739fbea2074e0a5c9a',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  const handleSignIn = async (code: string) => {
+    const response = await api.post('/register', {
+      code,
+    })
+
+    const { token } = response.data
+
+    await SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params
+      handleSignIn(code)
+    }
+  }, [response])
 
   if (!hasLoadedFonts) {
     return null
@@ -43,14 +87,14 @@ export default function App() {
             Sua cápsula do tempo
           </Text>
           <Text className="text-center font-body text-base leading-relaxed text-gray-100">
-            Colecione momentos marcantes da sua Quem lê da cu é viado, quem lê
-            da cu é viado, quem lê da cu é viado,
+            Colecione momentos marcantes da sua
           </Text>
         </View>
 
         <TouchableOpacity
           activeOpacity={0.7}
           className="px-y rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signInWithGithub()}
         >
           <Text className="font-alt text-sm font-bold uppercase text-black">
             Cadastrar lembrança
---
+++ mobile/src/lib/api.ts
@@ -0,0 +1,6 @@
+import axios from 'axios'
+
+export const api = axios.create({
+  baseURL:
+    'https://1f03-2804-41fc-802d-ed60-7013-fbab-7d2a-cd36.ngrok-free.app',
+})
