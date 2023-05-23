7ff366aec100f29fbeab45f966ea7a8d326f5a5d
rhuanbello <rhuambello@gmail.com>
2023-05-22 21:40:27 -0300

    [style] Mobile Homepage

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -49,11 +49,11 @@ export default function Home() {
           Feito com 💜 no NLW da{' '}
           <a
             target="_blank"
-            href="https://rocketseat.com.br"
+            href="https://github.com/rhuanbello"
             className="underline hover:text-gray-100"
             rel="noreferrer"
           >
-            Rocketseat
+            Rocketseat por @rhuanbello
           </a>
         </div>
       </div>
