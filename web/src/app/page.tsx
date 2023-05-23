5d143695a9e78d69d916d7afec9d85c46b103511
rhuanbello <rhuambello@gmail.com>
2023-05-23 19:36:10 -0300

    [feature] Auth Callback Route & Profile Logged User

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,13 @@
 import { CopyRight } from '@/components/Copyright'
 import { EmptyMemories } from '@/components/EmptyMemories'
 import { Hero } from '@/components/Hero'
+import { Profile } from '@/components/Profile'
 import { SignIn } from '@/components/SignIn'
+import { cookies } from 'next/headers'
 
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <main className="grid min-h-screen grid-cols-2">
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover  px-28 py-16">
@@ -11,7 +15,8 @@ export default function Home() {
 
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
-        <SignIn />
+        {isAuthenticated ? <Profile /> : <SignIn />}
+
         <Hero />
         <CopyRight />
       </div>
